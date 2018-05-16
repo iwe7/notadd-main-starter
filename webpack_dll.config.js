790a3251a257bd1e3957aee11aa4356fb4552a1c
iwe7 <admin@meepo.com.cn>
2018-05-16 15:08:55 +0800

    first commit

---
+++ webpack_dll.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    animations: ['@angular/animations', '@angular/platform-browser/animations'],
+    elements: ['@angular/elements'],
+    cdk: ['@angular/cdk'],
+    common: ['@angular/common'],
+    router: ['@angular/router'],
+    http: ['@angular/common/http'],
+    forms: ['@angular/forms'],
+    core: ['@angular/core'],
+    browser: ['@angular/platform-browser', '@angular/platform-browser-dynamic'],
+    rxjs: ['rxjs', 'rxjs/operators'],
+    moment: ['moment'],
+    lodash: ['lodash']
+  },
+  output: {
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'lisbs'),
+    library: '_dll_[name]',
+  },
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: "async",
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        commons: {
+          name: "meepo",
+          chunks: "initial",
+          minChunks: 2
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      context: __dirname,
+      name: "_dll_[name]",
+      path: path.join(__dirname, 'libs', "[name].manifest.json"),
+    })
+  ],
+  mode: "production"
+};
